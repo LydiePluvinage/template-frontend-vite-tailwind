fab21b7a9d2140b141c3c69311f4d9d60bca7a0a
Lydie Pluvinage <lydiepluvinage@yahoo.fr>
2022-04-25 16:57:03 +0200

    Test changement de text pour vérifier la synchronisation entre github et vercel

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="flex items-center justify-center w-screen h-screen text-2xl text-white bg-slate-500 text-shadow-md">
       <p className="w-2/4 text-center">
-        Voici comment déployer une application Vite avec Parcel
+        Mon site créé par Vite a été déployé sur Vercel avec succès !
       </p>
     </div>
   );
