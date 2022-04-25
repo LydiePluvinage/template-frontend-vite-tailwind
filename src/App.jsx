26157616c7f0e571346e54766af6d66b0b5a5396
Lydie Pluvinage <lydiepluvinage@yahoo.fr>
2022-04-25 16:46:08 +0200

    retour en arrière

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="flex items-center justify-center w-screen h-screen text-2xl text-white bg-slate-500 text-shadow-md">
       <p className="w-2/4 text-center">
-        Le projet Vite-Tailwind a été déployé grâce à Vercel !
+        Voici comment déployer une application Vite avec Parcel
       </p>
     </div>
   );
