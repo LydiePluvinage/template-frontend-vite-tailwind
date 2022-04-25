d35658f461f6d2dea7f530674117eb2a4bdeaa76
Lydie Pluvinage <lydiepluvinage@yahoo.fr>
2022-04-25 16:27:14 +0200

    Test push main pour voir les modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
+
 function App() {
   return (
     <div className="flex items-center justify-center w-screen h-screen text-2xl text-white bg-slate-500 text-shadow-md">
       <p className="w-2/4 text-center">
-        Voici comment déployer automatiquement un projet configuré avec Vite
-        grâce à Vercel
+        Le projet Vite-Tailwind a été déployé grâce à Vercel !
       </p>
     </div>
   );
