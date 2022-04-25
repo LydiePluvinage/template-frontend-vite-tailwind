98937a3a1d74696a7a44e5df6278dbfcdaecf8b2
Lydie Pluvinage <lydiepluvinage@yahoo.fr>
2022-04-25 16:17:30 +0200

    text pour vidéo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
 function App() {
   return (
-    <div className="flex items-center justify-center w-screen h-screen text-xl text-white bg-slate-500 text-shadow-md">
-      <p>
-        Ce template est bien configuré pour vos applications front-end avec Vite
-        et Tailwind
+    <div className="flex items-center justify-center w-screen h-screen text-2xl text-white bg-slate-500 text-shadow-md">
+      <p className="w-2/4 text-center">
+        Voici comment déployer automatiquement un projet configuré avec Vite
+        grâce à Vercel
       </p>
     </div>
   );
